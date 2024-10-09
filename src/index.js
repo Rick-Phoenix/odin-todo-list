445d5619e0becfbbfe26b2662f823d92fab1b14d
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-09 11:30:12 +0200

    Added localStorage interactions

--- src/index.js
+++ src/index.js
@@ -3,27 +3,63 @@ import * as sel from "./selectors.js";
 import close from "./Icons/close.svg";
 import add from "./Icons/addCheck.svg";
 
+const today = new Date().toISOString().split('T')[0];
+sel.date.setAttribute('min', today);
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        Task.loadFromLocalStorage(key)
+    }
+});
+
 sel.addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const title = sel.form.elements['title'].value;
     const date = sel.form.elements['date'].value;
     const priority = sel.form.elements['priority'].value;
     const description = sel.form.elements['description'].value;
-    const newTask = new Task(title, date, priority, description);
-    newTask.createTask();
+        if (sel.form.checkValidity()) {
+            const newTask = new Task(title, date, priority, description);
+            newTask.createTask();
+            newTask.saveToLocalStorage();
+        } else alert('Invalid form. Please fill the form with all the necessary values.');
+    
 })
 
 class Task {
     constructor(title, date, priority, description) {
         this.title = title
         this.date = date
-        this.priority = `${priority}/5`
+        this.priority = priority
         this.description = description
         this.checklist = []
         this.notes = [];
     }
 
-    
+    toJSON() {
+        return {
+            title: this.title,
+            date: this.date,
+            priority: this.priority,
+            description: this.description,
+            checklist: this.checklist,
+            notes: this.notes,
+        }
+    };
+
+    static loadFromLocalStorage(name) {
+        const task = JSON.parse(localStorage.getItem(name));
+        const newtask = new Task(task.title, task.date, task.priority, task.description, task.checklist, task.notes);
+        newtask.createTask();
+    }
+
+    saveToLocalStorage() {
+        const key = this.title;
+        const value = JSON.stringify(this);
+        localStorage.setItem(key, value);
+    }
+
     createTask() {
     const task = createElement('div', 'task');
 
@@ -38,7 +74,7 @@ class Task {
     
     const priority = createElement('div', 'priority');
     createPseudo(priority, 'Priority:');
-    priority.textContent = this.priority;
+    priority.textContent = `${this.priority}/5`;
     
     const taskDesc = createElement('div', 'taskDesc');
     createPseudo(taskDesc, 'Description:');
@@ -47,69 +83,21 @@ class Task {
     const list = createElement('div', 'checklist');
     createPseudo(list, 'To-do List:');
     const addCheck = createElement('button', 'addCheck');
-
-    
-
-
     addCheck.addEventListener('click', () => {
-    const modal = createElement('dialog');
-    const form = createElement('form');
-    const label = createElement('label');
-    const saveBtn = createElement('button', 'save');
-    label.textContent = 'Add a new task';
-    label.setAttribute = ('for', 'add');
-    const input = createElement('input');
-    input.setAttribute = ('id', 'add');
-    form.append(label, input, saveBtn);
-    saveBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        const value = input.value;
-        if (value == false) {
-            modal.close();
-            return;
-        };
-        this.checklist.push(input.value);
-        
-        const label = createElement('label');
-        label.setAttribute('for', `${value}`);
-        label.textContent = `${value}`;
-        const check = createElement('input');
-        check.setAttribute('type', 'checkbox');
-
-        check.addEventListener('change', () => {
-            if (check.checked == true) label.style.textDecoration = 'line-through';
-            else label.style.textDecoration = 'none';
-        });
-        const inputGroup = createElement('div', 'inputGroup');
-
-        inputGroup.append(label, check);
-        list.append(inputGroup);
-        modal.close();
-    });
-    modal.append(form);
-    list.append(modal);
-    modal.showModal()
-    });
-
-
-    list.append(addCheck);
-   
-
-
-    
-    const notes = createElement('div', 'notes');
-    createPseudo(notes, 'Notes:');
-    const addNote = createElement('button', 'addNote');
-    addNote.addEventListener('click', () => {
         const modal = createElement('dialog');
         const form = createElement('form');
+
         const label = createElement('label');
-        const saveBtn = createElement('button', 'save');
-        label.textContent = 'Add notes:';
+        label.textContent = 'Add a new task';
         label.setAttribute = ('for', 'add');
+
         const input = createElement('input');
         input.setAttribute = ('id', 'add');
+
+        const saveBtn = createElement('button', 'save');
+
         form.append(label, input, saveBtn);
+
         saveBtn.addEventListener('click', (e) => {
             e.preventDefault();
             const value = input.value;
@@ -117,28 +105,96 @@ class Task {
                 modal.close();
                 return;
             };
+
+            this.checklist.push(input.value);
+            
+            const label = createElement('label');
+            label.setAttribute('for', `${value}`);
+            label.textContent = `${value}`;
+
+            const check = createElement('input');
+            check.setAttribute('type', 'checkbox');
+
+            check.addEventListener('change', () => {
+                if (check.checked == true) label.style.textDecoration = 'line-through';
+                else label.style.textDecoration = 'none';
+            });
+
+            const inputGroup = createElement('div', 'inputGroup');
+
+            inputGroup.append(label, check);
+            list.append(inputGroup);
+            modal.close();
+        });
+
+        modal.append(form);
+        list.append(modal);
+
+        modal.showModal()
+    });
+
+    list.append(addCheck);
+   
+    const notes = createElement('div', 'notes');
+    createPseudo(notes, 'Notes:');
+    const addNote = createElement('button', 'addNote');
+
+    addNote.addEventListener('click', () => {
+        const modal = createElement('dialog');
+        const form = createElement('form');
+
+        const label = createElement('label');
+        label.textContent = 'Add notes:';
+        label.setAttribute = ('for', 'add');
+
+        const saveBtn = createElement('button', 'save');
+        
+        const input = createElement('input');
+        input.setAttribute = ('id', 'add');
+
+        form.append(label, input, saveBtn);
+        saveBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const value = input.value;
+
+            if (value == false) {
+                modal.close();
+                return;
+            };
+
             notesList.classList.remove('hidden');
             this.notes.push(value);
+
             const content = createElement('li', 'note');
             const contentFlex = createElement('span', 'liFlex');
             contentFlex.textContent = `${value}`;
+
             const removeBtn = createElement('button', 'remove');
-            contentFlex.append(removeBtn)
+            removeBtn.addEventListener('click', () => {
+                content.remove();
+                const index = this.notes.indexOf(value);
+                this.notes.splice(index, 1);
+                if (this.notes.length < 1 ) notesList.classList.add('hidden');
+            });
+
+            contentFlex.append(removeBtn);
             content.append(contentFlex);
             notesList.append(content);
             modal.close();
         });
+
         modal.append(form);
         notes.append(modal);
         modal.showModal()
-        })
+    });
+    
     notes.append(addNote);
     
     const removeBtn = createElement('button', 'remove');
-    removeBtn.style.backgroundImage = `url(${close})`;
     removeBtn.addEventListener('click', (e) => {
         e.preventDefault();
         task.remove();
+        localStorage.removeItem(this.title);
     });
 
     const notesList = createElement('ul', 'notesList', 'hidden');
@@ -146,9 +202,8 @@ class Task {
     task.append(taskHeader, taskDesc, list, notes, notesList);
     sel.board.append(task);
     };
-}
 
-const test = new Task('Do the laundry', 'Today', '3', 'Self-explanatory...', ['check1', 'check2', 'check3'], 'So exciting!');
+};
 
 function createElement(type, ...classes) {
     const element = document.createElement(type);
@@ -160,6 +215,3 @@ function createPseudo(target, value) {
     target.style.setProperty('--pseudo', `"${value}"`);
 }
 
-function addToDo() {
-    
-}
--- src/selectors.js
+++ src/selectors.js
@@ -3,3 +3,4 @@ export const desc = document.querySelector('.taskDesc');
 export const title = document.querySelector('.title');
 export const board = document.getElementById('taskboard');
 export const form = document.getElementById('newTask');
+export const date = document.getElementById('date');
